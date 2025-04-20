da9bd957647f01ab51c16f78fe36de7e76bc36a5
Ebenezer Adams <mobeston@gmail.com>
2025-04-19 23:48:34 -0400

    Add generate_compose.py for dynamic Docker Compose generation and update Dockerfile and requirements
    Initial changes docker workability

---
+++ generate_compose.py
@@ -0,0 +1,35 @@
+import yaml
+
+with open("configs/tickers.txt") as f:
+    tickers = [line.strip() for line in f]
+
+
+
+services = {}
+for ticker in tickers:
+    services[f"scale_t_bot_{ticker.lower()}"] = {
+        "build": {
+            "context": ".",
+            "dockerfile": f"main/bots/SCALE_T/Dockerfile",
+        },
+        "command": "tail -f /dev/null",
+        "container_name": f"scale_t_bot_{ticker.lower()}",
+        "volumes": [
+            f"./data/SCALE_T/ticker_data/paper/{ticker.upper()}.csv:/app/data/ticker_data/paper/{ticker.upper()}.csv",
+        ],
+        "restart": "no",
+        "logging": {
+            "driver": "json-file",
+            "options": {
+                "max-size": "10m",
+                "max-file": "3"
+            }
+        },
+    }
+
+compose = {
+    "services": services
+}
+
+with open("docker-compose.generated.yml", "w") as f:
+    yaml.dump(compose, f)
